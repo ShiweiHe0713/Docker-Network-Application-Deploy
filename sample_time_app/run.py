b6a10f1ba2a89b0296211d4e1048469e2695bc43
ShiweiHe0713 <67429121+ShiweiHe0713@users.noreply.github.com>
2023-12-22 15:34:14 -0500

    first commit

--- sample_time_app/run.py
+++ sample_time_app/run.py
@@ -1,12 +1,19 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/time')
+def returnTime():
+    from datetime import datetime
+    now=datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return now + "\n Shiwei He \n NetID: sh7787\n"
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello world!'
+    return returnTime()
 
 
 app.run(host='0.0.0.0',
         port=8080,
         debug=True)
+
